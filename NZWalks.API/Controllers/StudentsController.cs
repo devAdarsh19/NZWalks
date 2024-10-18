c6eb3d612e8a20a4c5c76299e294632cf9565cc3
Adarsh Vinod <adarshvinod12@gmail.com>
2024-10-18 14:18:41 +0530

    initial commit

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:portNumber/api/Student
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET : https://localhost:portNumber/api/Student
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] students = {"John", "Jane", "Luke", "Robin"};
+
+            return Ok(students);
+        }
+    }
+}
