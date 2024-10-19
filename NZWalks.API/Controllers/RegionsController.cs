47232b6df217811c61f8766ac047d4f6b9ceccc8
Adarsh Vinod <adarshvinod12@gmail.com>
2024-10-19 22:07:45 +0530

    Performed migrations and added RegionsController.cs

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    // GET:https://localhost:1234/api/Regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Auckland",
+                    Code = "AKL",
+                    RegionImageUrl = "https://images.squarespace-cdn.com/content/v1/59fea826fe54eff6e1e8ceb5/1558647678972-Q05SNU5ACBNN6LEVF4MP/Auckland+CBD+skytower.jpg"
+                },
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Wellington",
+                    Code = "WLG",
+                    RegionImageUrl = "https://wellingtonnz.bynder.com/transform/f2224523-265d-4578-a72a-b9b068d987ce/Mount-Victoria-Lookout"
+                }
+            };
+            return Ok(regions);
+        }
+    }
+}
--- NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6eb3d612e8a20a4c5c76299e294632cf9565cc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
